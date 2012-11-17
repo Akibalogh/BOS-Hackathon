6be29fef31f1a27bd516ce6262b2319ab2ffe0af
Aki Balogh <akibalogh@gmail.com>
2012-11-17 14:21:19 -0800

    updated shell script

--- run_it.sh
+++ run_it.sh
@@ -1,6 +1,7 @@
 HADOOP_HOME=/usr
 
 $HADOOP_HOME/bin/hadoop  jar /usr/lib/hadoop-0.20/contrib/streaming/hadoop-streaming-0.20.2-cdh3u5.jar \
+    -D mapred.reduce.tasks=20
     -mapper 'perl map.pl' \
     -reducer 'perl reduce.pl' \
     -input /datasets/msd \
